eba3e4dd95e8cb83894e1a2bf0255591d8769a41
Eunhye Kang <gracekang61@naver.com>
2022-03-12 22:23:58 +0900

    abstract/interface

---
+++ polymorphism/PolymorphismDemo1.java
@@ -0,0 +1,82 @@
+package org.opentutorials.javatutorials.polymorphism;
+
+/*
+
+class A {
+	public String x() {
+		return "x";
+	}
+}
+
+class B extends A {
+	public String y() {
+		return "y";
+	}
+}
+
+public class PolymorphismDemo1 {
+	public static void main(String[] args) {
+		A obj = new B();
+		obj.x();
+		//obj.y();
+	}
+}
+
+*/
+
+/*
+class A {
+	public String x() {
+		return "A.x";
+	}
+}
+
+class B extends A {
+	public String x() {
+		return "B.x";
+	}
+
+	public String y() {
+		return "y";
+	}
+}
+
+public class PolymorphismDemo1 {
+	public static void main(String[] args) {
+		A obj = new B();
+		System.out.println(obj.x());
+	}
+}
+
+*/
+
+class A {
+	public String x() {
+		return "A.x";
+	}
+}
+
+class B extends A {
+	public String x() {
+		return "B.x";
+	}
+
+	public String y() {
+		return "y";
+	}
+}
+
+class B2 extends A {
+	public String x() {
+		return "B2.x";
+	}
+}
+
+public class PolymorphismDemo1 {
+	public static void main(String[] args) {
+		A obj = new B();
+		A obj2 = new B2();
+		System.out.println(obj.x());
+		System.out.println(obj2.x());
+	}
+}
